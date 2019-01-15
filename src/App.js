63a6ea4a05a8c2e7b22c19884f02a8092ea90b35
Siegel <csiegel@dmu.edu>
2019-01-14 20:18:59 -0600

    first commit.  not much to see.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function User(props) {
+	return (
+		<button className="user">
+		    {props.SOMETHING TODO}
+		</button>
+    );
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	
+	constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+       }
+    }
+	
+	componentDidMount() {
+        fetch('https://bank-account-microservice.herokuapp.com/api/users/')
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					isLoaded: true,
+					items: json
+				})
+			});
+    }
+	
+	render() {
+		
+		var { isLoaded, items } = this.state; 
+	
+		if (!isLoaded) {
+			return <div>Loading...</div>
+		}
+	
+		return (
+		<div className="App">
+			<ul>
+				{items.map(item => (
+					<li key={item.id}>
+						Name: {item.name} | Email: {item.email}
+					</li>
+				))};
+			</ul>
+		</div>
+	);
+	}
 }
 
 export default App;
