3e82e28439ee1f370a0058a74e20a5fac7ae1861
Siegel <csiegel@dmu.edu>
2019-01-15 10:54:45 -0600

    display all users and accounts.  Next step is to make them editable and createable.  Also need to pretty things up just a bit.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,85 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "./App.css"
 
-function User(props) {
-	return (
-		<button className="user">
-		    {props.SOMETHING TODO}
-		</button>
-    );
+class Account extends Component {
+	
+	constructor(props) {
+        super(props);
+        this.state = {
+            account: [],
+            isLoaded: false,
+       }
+    }
+	componentDidMount() {
+		fetch('https://cors-anywhere.herokuapp.com/' + this.props.data)
+				.then(res => res.json())
+				.then(json => {
+					this.setState({
+						isLoaded: true,
+						account: json
+					})
+				});
+	}
+			
+	render() {
+		const { isLoaded, account } = this.state;
+		if (account.length === 0) {
+			return(<div className="emptyAccount"></div>)
+		} else {
+			return (
+				<div className="account">
+					Account Type: {account.account_type} | 
+					Balance: {account.balance} | 
+					Interest Rate: {account.interest_rate} | 
+					Opened On: {account.account_opened.split("T")[0]}
+				</div>
+			);
+		}
+	}
 }
+
+
+class User extends Component {
+	
+	render() {
+		return (
+			<div className="user">
+				username: {this.props.data.username} | email: {this.props.data.email} <br />
+				accounts: {this.props.data.accounts.map(account => (
+					<ul key={account.id}>
+						<Account data={account}/>
+					</ul>
+				))}
+			</div>
+		);
+	}
+}
+
 
 class App extends Component {
 	
 	constructor(props) {
         super(props);
         this.state = {
-            items: [],
+            users: [],
             isLoaded: false,
        }
     }
 	
 	componentDidMount() {
-        fetch('https://bank-account-microservice.herokuapp.com/api/users/')
+        fetch('https://cors-anywhere.herokuapp.com/https://bank-account-microservice.herokuapp.com/api/users/')
 			.then(res => res.json())
 			.then(json => {
 				this.setState({
 					isLoaded: true,
-					items: json
+					users: json
 				})
 			});
     }
 	
 	render() {
-		
-		var { isLoaded, items } = this.state; 
+		const { isLoaded, users } = this.state; 
 	
 		if (!isLoaded) {
 			return <div>Loading...</div>
@@ -40,11 +88,11 @@ class App extends Component {
 		return (
 		<div className="App">
 			<ul>
-				{items.map(item => (
-					<li key={item.id}>
-						Name: {item.name} | Email: {item.email}
+				{users.map(user => (
+					<li key={user.username}>
+						<User data={user}/>
 					</li>
-				))};
+				))}
 			</ul>
 		</div>
 	);
